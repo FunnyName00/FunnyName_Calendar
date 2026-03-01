955b1c8732cf87df15259c90bc8100f20e9452fe
FunnyName <emi.houdart@student.uclouvain.be>
2026-03-01 10:20:21 +0100

    Added Flask and example

--- python/ListTest.py
+++ python/ListTest.py
@@ -1,14 +1,14 @@
-import ListFunctions as Ls
+from ListFunctions import *
 
-list = Ls.GeneralList()
-item1 = Ls.GeneralItem("Papa", "Manger :3")
-item2 = Ls.GeneralItem("Maman", "PQ")
+list = GeneralList()
+item1 = GeneralItem("Papa", "Manger :3")
+item2 = GeneralItem("Maman", "PQ")
 
 list.addItem(item1)
 list.addItem(item2)
 list.print()
 
-activity1 = Ls.Activity("Papa", "ENORME TEUFFFFF", "05/03/26", "Louvain la neuve")
+activity1 = Activity("Papa", "ENORME TEUFFFFF", "05/03/26", "Louvain la neuve")
 list.addItem(activity1)
 list.print()
 
