546a1d353344709e7832df69a77995da4eccc623
FunnyName <emi.houdart@student.uclouvain.be>
2026-03-04 11:18:36 +0100

    added login screen and functionality, username is automatically put as user in list item

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 # Create a db file in project folder
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -35,12 +36,27 @@ with app.app_context():
 def index():
     # Query all items from the database instead of a list
     items = GeneralItem.query.all()
+    
     return render_template('index.html', items=items)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
 
 @app.route('/add', methods=['POST'])
 def add_item():
     item_type = request.form.get('type')
-    user = request.form.get('user')
+    user = session['username'] 
     content = request.form.get('content')
 
     new_item = GeneralItem(user=user, content=content, item_type=item_type)
